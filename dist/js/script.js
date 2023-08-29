b8d2bfe809a5033a9e05432706ad3fc30bf9442c
Adrian Manczak <manczakadrian@gmail.com>
2023-08-30 00:18:34 +0200

    processOrderupdate

--- dist/js/script.js
+++ dist/js/script.js
@@ -194,87 +194,87 @@
       thisProduct.priceElem.innerHTML = price;
 
     }
-        }
-
-      }
-
-      console.log(thisProduct);
-    }
-
-  class AmountWidget {
-    constructor(element) {
-      const thisWidget = this;
-      thisWidget.getElements(element);
-      thisWidget.setValue(thisWidget.input.value);
-
-
-
-      console.log('AmountWidget:', thisWidget);
-      console.log('constructor arguments:', element);
-
-    }
-    getElements(element) {
-      const thisWidget = this;
-
-      thisWidget.element = element;
-      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-    }
-    setValue(value) {
-      const thisWidget = this;
-      const newValue = parseInt(value);
-
-      if (thisWidget.value !== newValue) {
-        thisWidget.value = newValue;
-      }
-      if (thisWidget.value !== newValue && !isNaN(newValue)) {
-        thisWidget.value = newValue;
-
-      }
-      thisWidget.value = newValue;
-      thisWidget.input.value = thisWidget.value;
-    }
-
-    initActions() {
-
-    }
-  }
-  const app = {
-    initMenu: function () {
-      const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
-
-      for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
-      }
-
-      // const testproduct = new Product();
-      // console.log('testProduct:', testproduct);
-    },
-
-    initData: function () {
-      const thisApp = this;
-      thisApp.data = dataSource;
-
-    },
-
-    init: function () {
-      const thisApp = this;
-
-
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
-
-      thisApp.initData();
-      thisApp.initMenu();
-
-    },
-  };
-
-  app.init();
+  
 
 }
+
+// console.log(thisProduct);
+
+
+class AmountWidget {
+  constructor(element) {
+    const thisWidget = this;
+    thisWidget.getElements(element);
+    thisWidget.setValue(thisWidget.input.value);
+
+
+
+    console.log('AmountWidget:', thisWidget);
+    console.log('constructor arguments:', element);
+
+  }
+  getElements(element) {
+    const thisWidget = this;
+
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+  }
+  setValue(value) {
+    const thisWidget = this;
+    const newValue = parseInt(value);
+
+    if (thisWidget.value !== newValue) {
+      thisWidget.value = newValue;
+    }
+    if (thisWidget.value !== newValue && !isNaN(newValue)) {
+      thisWidget.value = newValue;
+
+    }
+    thisWidget.value = newValue;
+    thisWidget.input.value = thisWidget.value;
+  }
+
+  initActions() {
+
+  }
+}
+const app = {
+  initMenu: function () {
+    const thisApp = this;
+    console.log('thisApp.data:', thisApp.data);
+
+    for (let productData in thisApp.data.products) {
+      new Product(productData, thisApp.data.products[productData]);
+    }
+
+    // const testproduct = new Product();
+    // console.log('testProduct:', testproduct);
+  },
+
+  initData: function () {
+    const thisApp = this;
+    thisApp.data = dataSource;
+
+  },
+
+  init: function () {
+    const thisApp = this;
+
+
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
+
+    thisApp.initData();
+    thisApp.initMenu();
+
+  },
+};
+
+app.init();
+}
+

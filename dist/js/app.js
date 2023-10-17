2a80079b9b9887dbea6e0fba1d725b7328379882
Adrian Manczak <manczakadrian@gmail.com>
2023-10-18 00:49:52 +0200

    Projectmoduleupdate

--- dist/js/app.js
+++ dist/js/app.js
@@ -43,7 +43,7 @@ import CartProduct from "./Components/CartProduct.js";
       thisApp.cart = new Cart(cartElem);
       thisApp.productList=document.querySelector(select.containerOf.menu);
       thisApp.productList.addEventListener('add-to-cart', function(event){
-        app.cart.add(event.detail.product);
+        app.cart.add(event.detail.thisProduct.prepareCartProduct);
       })
     },
     init: function () {
